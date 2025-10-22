aed3e9aaef244be9ffca2b3fac70fc91b93c3147
Aditya Dey <168162359+AdityaWorkmates@users.noreply.github.com>
2025-10-22 18:01:53 +0530

    Update aws_agent_app.py

--- aws_agent_app.py
+++ aws_agent_app.py
@@ -35,11 +35,11 @@ load_dotenv()
 # For deploying in EC2 or ECS
 
 # boto3 session (uses env or IAM role)
-# session = boto3.Session(
-#     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
-#     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-#     region_name=os.getenv("REGION_NAME"),
-# )
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("REGION_NAME"),
+)
 
 # For deploying in streamlit
 
@@ -49,6 +49,9 @@ session = boto3.Session(
     aws_secret_access_key=st.secrets.get("AWS_SECRET_ACCESS_KEY"),
     region_name=st.secrets.get("AWS_DEFAULT_REGION", "us-west-2"),
 )
+
+print(aws_access_key)
+print(region_name)
 
 
 HISTORY_FILE = "aws_agent_history.json"
